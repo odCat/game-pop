14693d3279f6eec5a01d7cfc272e8f21777e5ab8
Mihai Gătejescu <mgatejescu@pm.me>
2025-05-03 20:48:29 +0300

    Put the response in a dictionary

--- game-pop.py
+++ game-pop.py
@@ -1,7 +1,29 @@
-import requests
-import json
+#! python
 
-r = requests.get("https://esi.evetech.net/latest/status/?datasource=tranquility")
-print(r)
-print(json.loads(r.text)['players'])
+from datetime import date as dt
+import time
+import requests
+
+
+def make_request():
+    return requests.get("https://esi.evetech.net/latest/status/?datasource=tranquility")
+
+
+def entry(response):
+    game = "EVE Online"
+    players = response.json()["players"]
+    date = dt.today().isoformat()
+    hour = time.strftime("%H", time.localtime())
+
+    return {
+            "game": game,
+            "players": players,
+            "date": date,
+            "hour": hour
+        }
+
+if __name__ == "__main__":
+    print(entry(make_request()))
+
+
 
